f76727ff5d9b86bd17a73c48903972c075694408
funnytao <funnytao@bu.edu>
2016-08-19 18:30:48 -0400

    updated chat function

--- src/app/shared/global.service.ts
+++ src/app/shared/global.service.ts
@@ -9,19 +9,13 @@ export class GlobalService {
   username: string;
   uid: string;
   ticket_number: number;
-<<<<<<< HEAD
   imgUrl: string;
-=======
->>>>>>> ef350b95d65b9ad28f7f91bb006de25ee747b507
   constructor() {
     this.toggle = false;
     this.navClicked = false;
     this.width = window.innerWidth;
     this.username = "USER";
     this.ticket_number = 0;
-<<<<<<< HEAD
     this.imgUrl = 'http://cdn.bleedingcool.net/wp-content/uploads/2012/09/sam-bain-twitter-avatar.jpeg';
-=======
->>>>>>> ef350b95d65b9ad28f7f91bb006de25ee747b507
   }
 }
